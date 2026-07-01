e894ab4e0b5d833c63d344f00d2bf1f34a77feac
sawada-fumiya <f238238238238@gmail.com>
2026-07-01 16:53:33 +0900

    Update scraper to use ZenRows API

--- backend/src/lib/scraper.js
+++ backend/src/lib/scraper.js
@@ -2,23 +2,12 @@ import * as cheerio from 'cheerio';
 
 export async function scrapeCups(targetMonth = null, targetDows = []) {
   try {
-    const res = await fetch('https://labola.jp/r/event/3014/tournament', {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'ja,en-US;q=0.9,en;q=0.8',
-        'Cache-Control': 'no-cache',
-        'Referer': 'https://labola.jp/',
-        'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1'
-      }
-    });
+    const targetUrl = encodeURIComponent('https://labola.jp/r/event/3014/tournament');
+    const zenRowsApiKey = '1710b358a28644f03a1cc0b017e59ba81492686c';
+    const zenRowsUrl = `https://api.zenrows.com/v1/?apikey=${zenRowsApiKey}&url=${targetUrl}`;
+    
+    console.log("Fetching from ZenRows API...");
+    const res = await fetch(zenRowsUrl);
     const status = res.status;
     const html = await res.text();
     const $ = cheerio.load(html);
